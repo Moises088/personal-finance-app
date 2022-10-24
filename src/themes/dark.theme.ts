c9eea46b546791a5408b51db803fd07cdf94eec9
Moises088 <costamoises300@gmail.com>
2022-10-24 20:17:27 -0300

    feat: create themes

---
+++ src/themes/dark.theme.ts
@@ -0,0 +1,6 @@
+import { ThemesConfig } from "../interfaces/themes.interface";
+import { DefaultTheme } from "./default.theme";
+
+export const DarkTheme: ThemesConfig = {
+    ...DefaultTheme
+}
---
+++ src/themes/default.theme.ts
@@ -0,0 +1,18 @@
+import { ThemesConfig } from "../interfaces/themes.interface";
+
+export const DefaultTheme: ThemesConfig = {
+    background: {
+        primary: "#141b26",
+        secondary: "#3b4045",
+        tertiary: "#1d2430"
+    },
+    text: {
+        primary: "#FFF",
+        secondary: "#000"
+    },
+    button: {
+        primary: "#F9603B",
+        secondary: "transparent"
+    }
+
+}
