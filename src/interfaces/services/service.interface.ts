4ab7851185ec7c91bbbfe99f8312c314422c220a
Moises088 <costamoises300@gmail.com>
2022-11-10 00:04:06 -0300

    feat: created comments in Services interfaces

--- src/interfaces/services/service.interface.ts
+++ src/interfaces/services/service.interface.ts
@@ -1,6 +1,27 @@
 export interface Services<T> {
+    /**
+     * Find all itens.
+     * @returns A Promise for itens array.
+    */
     find(): Promise<T[]>;
+
+    /**
+     * Find specific item.
+     * @param id To find specific item by id.
+     * @returns A Promise for specific item.
+    */
     findOne(id: number): Promise<T | undefined>;
+
+    /**
+     * Create a new item.
+     * @param createDto Body needed to create a new item.
+     * @returns A Promise for new item.
+    */
     create(createDto: T): Promise<T>;
+
+    /**
+     * Update item.
+     * @param updateDto Body needed to update item.
+    */
     update(updateDto: T): Promise<void>;
 }
