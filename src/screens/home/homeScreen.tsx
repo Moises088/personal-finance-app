bf260eaee19151378042e8f09a605396ae90c8bd
Moises088 <costamoises300@gmail.com>
2022-12-03 11:25:12 -0300

    feat: chart bar

--- src/screens/home/homeScreen.tsx
+++ src/screens/home/homeScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { SafeAreaView, ScrollView } from 'react-native';
+import { SafeAreaView, ScrollView, Image } from 'react-native';
+import BarChart from '../../components/global/bar-chart';
 import HomeBalance from '../../components/home/home-balance';
 import HomeBudget from '../../components/home/home-budget';
 import HomeFinanceHistoric from '../../components/home/home-finance-historic';
 import HomeHeader from '../../components/home/home-header';
+import { DEBTS_INSTITUTION } from '../../constants/debts.constants';
 import { ThemeContext } from '../../contexts/themeContext';
 import { styles } from './styles';
 
@@ -12,6 +14,8 @@ const HomeScreen: React.FC = () => {
   const { theme } = React.useContext(ThemeContext);
   const style = styles(theme);
 
+  const teste = DEBTS_INSTITUTION.map((debt, i) => ({ icon: <Image source={debt.logo} style={{ width: 30, height: 30, borderRadius: 5, marginBottom: 3 }} />, total: i + 100, color: debt.color }))
+
   return (
     <SafeAreaView style={style.container}>
       <HomeHeader />
@@ -19,6 +23,7 @@ const HomeScreen: React.FC = () => {
         <HomeBalance />
         <HomeBudget />
         <HomeFinanceHistoric />
+        <BarChart itens={teste} />
       </ScrollView>
     </SafeAreaView>
   );
